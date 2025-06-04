ad20a8a31fa388034bd829dfe4bc740c5e89826d
allisoneur88 <adganshin@gmail.com>
2025-06-04 14:30:54 +0200

    7_1_Added healthchecks for docker-compose; Scaffolded and stripped down next.js frontend app

---
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <header className="sticky top-0 z-50 flex">
+            <div>Left</div>
+            <div>Middle</div>
+            <div>Right</div>
+        </header>
+    )
+}
